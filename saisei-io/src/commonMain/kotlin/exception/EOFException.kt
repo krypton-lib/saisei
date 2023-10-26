7c72bd732d85e098e04e09f1361d99679183035f
viztea <hi@vzt.gay>
2023-10-26 04:28:51 -0700

    fix(io): java EOFException doesn't support causes

--- saisei-io/src/commonMain/kotlin/exception/EOFException.kt
+++ saisei-io/src/commonMain/kotlin/exception/EOFException.kt
@@ -8,9 +8,9 @@ package saisei.io.exception
 public expect class EOFException : IOException {
     public constructor()
 
-    public constructor(cause: Throwable?)
+//    public constructor(cause: Throwable?)
 
     public constructor(message: String?)
 
-    public constructor(message: String?, cause: Throwable?)
+//    public constructor(message: String?, cause: Throwable?)
 }
--- saisei-io/src/nativeMain/kotlin/exception/EOFException.kt
+++ saisei-io/src/nativeMain/kotlin/exception/EOFException.kt
@@ -8,9 +8,9 @@ package saisei.io.exception
 public actual class EOFException : IOException {
     public actual constructor() : super()
 
-    public actual constructor(cause: Throwable?) : super(cause)
+//    public actual constructor(cause: Throwable?) : super(cause)
 
     public actual constructor(message: String?) : super(message)
 
-    public actual constructor(message: String?, cause: Throwable?) : super(message, cause)
+//    public actual constructor(message: String?, cause: Throwable?) : super(message, cause)
 }
